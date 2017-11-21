e54cc6977c7e12262772f743c86aa317b38b5846
CelineDelbecque <31658420+CelineDelbecque@users.noreply.github.com>
2017-11-21 15:22:41 +0100

    init

---
+++ subwayworld.js
@@ -0,0 +1,57 @@
+Vue.component('bar-chart', {
+  extends: VueChartJs.Bar,
+  props: ['data', 'options'],
+  mounted () {
+    var data = this.data || {
+      labels: ["AB", "BC","MB","NB","NL","NS","NT","ON","PE","QC","SK","YT","NONE Specified"],
+      datasets: [
+        {
+          label: 'Number of victims of text-message fraud by province (Canada)',
+          backgroundColor: '#f87979',
+          data: [60,18,10,6,11,0,348,2,189,25,0,6],
+          datalabels: {
+            align: 'end',
+            anchor: 'end'
+          }
+        }
+      ]
+    };
+
+    var options = this.options || {
+      tooltips: {
+        enabled: false
+      },
+      legend: {
+        display: false
+      },
+			plugins: {
+				datalabels: {
+          display: true,
+					color: '#c46060',
+					font: {
+						weight: 'bold'
+					},
+					formatter: Math.round
+				}
+			},
+      scales: {
+        yAxes:  [
+          {
+            display: false,
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ],
+        xAxes: [
+          {
+            categoryPercentage: 1,
+            barPercentage: 2/3
+          }
+        ]
+      }
+    };
+
+    this.renderChart(data, options);
+  }
+})
